0d7521fa7acf980950f0668bfa0a9e4c1f9a17db
LenaG <mail@nomeans.net>
2013-11-29 15:47:08 -0800

    pancake recipe converted into dict

--- pancakes.py
+++ pancakes.py
@@ -1,22 +1,15 @@
+#makes 8 servings 
 
-makes 8 servings 
+recipe = dict(
+        flour = [1 + 1/2, 'cup'],
+        sugar = [1, 'tbsp'],
+        salt = [1, 'tsp'],
+        milk = [1 + 1/4, 'cup'],
+        vanilla = [1/2, 'tsp']
+        baking_powder = [3 + 1/2, 'tsp']
+        egg = [1, 'count']
+        butter_melted = [3, 'tbsp']
+        )
 
-    1 1/2 cups all-purpose flour
-
-    3 1/2 teaspoons baking powder
-
-    1 teaspoon salt
-
-    1 tablespoon white sugar
-
-    1 1/4 cups milk
-
-    1 egg
-
-    3 tablespoons butter, melted
-
-Directions
-
-    In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.
-    Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.
+# Directions: Mix flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth. Heat a lightly oiled pan over medium high heat. Pour 1/4 cup batter per pancake onto griddle.
 
